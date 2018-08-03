bbc46701487811d7670798e8c57196a45da419fd
Josh Balduff <joshmello@users.noreply.github.com>
2018-08-03 00:25:00 -0500

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -12,6 +12,8 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactNative = require('react-native');
 
+var PropTypes = require('prop-types');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -90,11 +92,11 @@ var Hr = function (_Component) {
 }(_react.Component);
 
 Hr.propTypes = {
-    lineStyle: _react.PropTypes.shape({}),
-    text: _react.PropTypes.string,
-    marginLeft: _react.PropTypes.number,
-    marginRight: _react.PropTypes.number,
-    textStyle: _react.PropTypes.shape({})
+    lineStyle: PropTypes.shape({}),
+    text: PropTypes.string,
+    marginLeft: PropTypes.number,
+    marginRight: PropTypes.number,
+    textStyle: PropTypes.shape({})
 };
 
 Hr.defaultProps = {
